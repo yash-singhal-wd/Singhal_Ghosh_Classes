42126e2838b9b016ca5bb878defa2302906020a1
Yash Singhal <yash98singhal@gmail.com>
2024-01-25 16:15:13 +0530

    print solution added

--- Recursion/Magic_method/cw-1/print.cpp
+++ Recursion/Magic_method/cw-1/print.cpp
@@ -6,10 +6,16 @@ using namespace std;
 
 void print_1_to_n(int n){
     // add your code here
+    if(n==0) return;
+    print_1_to_n(n-1);
+    cout << n << " ";
 }
 
 void print_n_to_1(int n){
     // add your code here
+    if(n==0) return;
+    cout << n << " ";
+    print_n_to_1(n-1);
 }
 
 int main(){
@@ -18,5 +24,6 @@ int main(){
     print_1_to_n(n);
     cout << endl;
     print_n_to_1(n);
+    cout << endl;
     return 1;
 }
